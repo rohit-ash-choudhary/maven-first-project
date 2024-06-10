38c50cfeb948b0f30a1e5ec78323d25c77e966eb
rohit-ash-choudhary <Rooh@#2001S>
2024-06-10 15:45:49 +0530

    first maven

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "--------------------project start -----------" );
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println( "---------------------project end -----------" );
+    }
+}
